55a521f15625f25f22e7b3d5e7b293c553ca3202
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-21 09:42:31 -0700

    120. LazyLoad - Carga perezosa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   },
   {
     path: 'countries',
-    component: AboutPageComponent
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule )
   },
   {
     path: '**',
