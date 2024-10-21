40d76dbaa1f60957e5862280d743d99b7e017d4d
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-21 10:53:32 -0700

    122. Funcionamiento del SearchBox

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -9,4 +9,12 @@ export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue( tag: string ) {
+    this.onValue.emit( tag );
+  }
+
 }
