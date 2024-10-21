080ab61be6ea1011c4f3234ee4180605fd74fb04
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-21 08:42:18 -0700

    116. Router Module y Páginas - SPA

---
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  template: 'jelouda',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+
+}
