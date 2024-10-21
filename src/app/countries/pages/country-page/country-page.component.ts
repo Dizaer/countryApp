0c37f3fee248020c106120a32c0a7cd26d4ba87a
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-21 14:40:30 -0700

    128. Argumentos por URL - Página de País

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+
+  constructor( 
+    private activedRoute: ActivatedRoute, 
+    private countryService: CountriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.activedRoute.params
+    .subscribe( ({ id }) => {
+
+      this.countryService.searchCountryByAlphaCode( id )
+      .subscribe( res => {
+        console.log( res );
+      })
+
+    });
+  }
+
 
 }
