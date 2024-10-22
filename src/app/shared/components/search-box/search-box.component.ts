0964882d51eb1fae8bc2a1e3248825c28aba1662
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-22 10:19:17 -0700

    140. Limpieza de suscripciones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,15 @@
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent implements OnInit{
+export class SearchBoxComponent implements OnInit, OnDestroy{
 
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -17,13 +18,17 @@ export class SearchBoxComponent implements OnInit{
   public onValue = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSubscription = this.debouncer
     .pipe(
       debounceTime(300)
     )
     .subscribe( value => {
       this.onValue.emit( value );
     })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();  
   }
 
   onKeyPress( searchTerm: string) {
