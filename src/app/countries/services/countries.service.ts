3e7f4d087140d5b637a21335cf90f28ebdcb8836
IE Dev 2 <ie.dev.2@hotmail.com>
2024-10-22 10:44:43 -0700

    142. Mantener la data entre pantallas

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
     private apiUrl: string = 'https://restcountries.com/v3.1';
+    public cacheStore: CacheStore = {
+        byCapital: { term: '', countries: [] },
+        byName: { term: '', countries: []},
+        byRegion: { term: '', countries: []}
+    }
 
     constructor(private httpClient: HttpClient) { }
 
